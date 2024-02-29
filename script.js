085e5bdddcd5537a0612e7a06f82be11c5c8d650
unknown <eskicmihajlo55@gmail.com>
2024-02-29 23:20:57 +0100

    added js with regex

---
+++ script.js
@@ -0,0 +1,42 @@
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const results= document.getElementById("results-div");
+
+const firstRegex = /^1 \d{3}-\d{3}-\d{4}$/
+
+const regexArray = [firstRegex];
+
+const test = (num) =>  regexArray.some((regex)=> regex.test(num))
+
+
+checkBtn.addEventListener("click" , ()=>{
+
+  if(userInput.value==="")
+  {
+    alert("Please provide a phone number");
+    return;
+
+  }
+
+  results.innerHTML += test(userInput.value) ? "Valid US number: " + userInput.value + "<br>"
+  : "Invalid US number: " + userInput.value + "<br>";
+
+
+
+
+
+
+})
+
+
+
+
+
+clearBtn.addEventListener("click", ()=>{
+
+
+    results.innerHTML = "";
+
+
+})
